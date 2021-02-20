32c79962ddaccc4c33400065a5297e5ba0795783
Niyaz <niyazzif@gmail.com>
2021-02-20 16:02:58 +0500

    add burger script

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,35 +1,29 @@
 
 import {getResource} from '../services/requests';
 
-
-
-
-
-
-
 const showMoreStyles = (trigger, wrapper) => {
     const btn = document.querySelector(trigger);
 
-        //   cards.forEach(card => {
-        //     card.classList.add('animated', 'fadeInUp');
-        // });
-    
-        // btn.addEventListener('click', () => {
-        //     cards.forEach(card => {
-        //         card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-        //         card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
-        //     });
-        //     // btn.style.display = 'none';
-        //     btn.remove();
-        // });
-    
+    // cards.forEach(card => {
+    //     card.classList.add('animated', 'fadeInUp');
+    // });
+
+    // btn.addEventListener('click', () => {
+    //     cards.forEach(card => {
+    //         card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
+    //         card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+    //     });
+    //     // btn.style.display = 'none';
+    //     btn.remove();
+    // });
+
     btn.addEventListener('click', function() {
-        getResource('http://localhost:3000/styles')
-        .then(res => createCards(res));
+        getResource('assets/db.json')
+            .then(res => createCards(res.styles))
+            .catch(error => console.log(error));
 
         this.remove();
     });
-
 
     function createCards(response) {
         response.forEach(({src, title, link}) => {
@@ -48,9 +42,6 @@ const showMoreStyles = (trigger, wrapper) => {
             document.querySelector(wrapper).appendChild(card);
         });
     };
-
-    
-
 };
 
 export default showMoreStyles;
